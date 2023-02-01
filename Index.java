790ab8771e10497e1ae9079656fb50908497d1c3
KonstantinVojt <114500793+KonstantinVojt@users.noreply.github.com>
2023-02-01 18:20:34 +0300

    Add files via upload

---
+++ Index.java
@@ -0,0 +1,64 @@
+package algorithms;
+
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Index {
+    public static void main(String[] args) {
+
+//        Level 1
+
+        System.out.println("Level 1.");
+        System.out.println("");
+        System.out.println("Введите 7 цифр в массив");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = new int[7];
+        int[] index = new int[7];
+
+        for (int i = 0; i <= arr.length - 1; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        for (int i = 0; i <= index.length - 1; i++) {
+            index[i] += i;
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+
+        System.out.println("Введите цифру из вашего массива индекс, которого хотите найти");
+        int num = scanner.nextInt();
+
+        for (int i = 0; i <= index.length - 1; i++) {
+            if (arr[i] == num) {
+                System.out.println("Индекс элемента: " + index[i]);
+            }
+
+        }
+
+        System.out.println("");
+
+//        Level 2
+
+        System.out.println("Level 2.");
+        System.out.println("");
+        System.out.println("Квадрат каждого элемента массива: ");
+        for (int i = 0; i <= arr.length - 1; i++) {
+            arr[i] *= arr[i];
+
+        }
+        System.out.println(Arrays.toString(arr));
+
+
+        System.out.println("Сумма квадратов всех элементов массива: ");
+        int sum = 0;
+        for (int i = 0; i <= arr.length - 1; i++) {
+            sum += arr[i];
+
+        }
+        System.out.println(sum);
+    }
+
+}
